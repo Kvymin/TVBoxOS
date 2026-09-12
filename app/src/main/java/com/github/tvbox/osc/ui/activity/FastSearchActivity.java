fccea4a0488bc4c75b4d144253a391a37c1ee0dd
21561 <215613905@qq.com>
2026-09-13 00:27:33 +0800

    搜索页历史词动态列宽

--- app/src/main/java/com/github/tvbox/osc/ui/activity/FastSearchActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/FastSearchActivity.java
@@ -67,7 +67,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class FastSearchActivity extends BaseActivity {
     private static final int SEARCH_THREAD_COUNT = 6;
-    private static final int SEARCH_MAX_THREAD_COUNT = Build.VERSION.SDK_INT >= 30 ? 18 : 12;
+    private static final int SEARCH_MAX_THREAD_COUNT = Build.VERSION.SDK_INT > 30 ? 18 : 12;
     private static final int SEARCH_PUMP_SECONDS = 2;
     private static final int SEARCH_NEXT_BATCH_SECONDS = 3;
     private static final int SEARCH_SITE_TIMEOUT_SECONDS = 10;
